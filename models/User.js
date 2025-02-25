40c6c4aedccc04d5cce01840d7c69aa73fec2923
Tyler VanSickle <tylervansickle@Tylers-MacBook-Pro.local>
2025-02-25 14:46:12 -0700

    Added basics of DB and server

---
+++ models/User.js
@@ -0,0 +1,22 @@
+const mongoose = require("mongoose");
+
+const UserSchema = new mongoose.Schema({
+  firstName: {
+    type: String,
+    required: true,
+  },
+  lastName: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+  },
+  age: {
+    type: Number,
+    required: true,
+  },
+});
+
+exports.User = mongoose.model("User", UserSchema);
---
+++ server.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const User = require("./models/User");
+
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+app.use(bodyParser.json());
+
+mongoose
+  .connect("mongodb://localhost:27017/", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("Connected to MongoDB");
+  })
+  .catch((err) => {
+    console.log("Failed to connect to MongoDB", err);
+  });
+
+
+app.get("/users", async (req, res) => {
+    const users = await User.find();
+    res.json(users);
+})
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
